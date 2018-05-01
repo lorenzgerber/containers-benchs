33c71b6390faa010235ba0c43d942e11cb5d9e3a
Lorenz Gerber <dv16lgr@cs.umu.se>
2018-05-01 14:07:09 +0000

    adding memory stream benchmark for singularity

---
+++ memory/singularity/singularity_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+## Clean the filesystem cache
+sync && free > tmp.out
+
+for i in {1..10}
+do
+	 	sudo singularity exec stream.img stream_benchmark > tmp.out && sed -n '27,31 p' tmp.out >> docker_stream.out
+			sync && free > tmp.out
+		done
